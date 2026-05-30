b7e0ee3d07febf5a0fedd283b8066c66fc9cde58
x-krizn <x.krizn@gmail.com>
2026-05-30 16:56:02 +0000

    Deploying to gh-pages from @ x-krizn/Orion-v2@d99ea28ef4c2d3aea00382ceb6cde2c783b44664 🚀

--- assets/volumetricLightingRenderVolume.vertex-DRf6JCqT.js
+++
@@ -1,5 +0,0 @@
-import{S as e,aC as n,aq as s}from"./index-WYExryXy.js";const t="volumetricLightingRenderVolumeVertexShader",r=`#include<sceneUboDeclaration>
-#include<meshUboDeclaration>
-attribute position : vec3f;varying vWorldPos: vec4f;@vertex
-fn main(input : VertexInputs)->FragmentInputs {let worldPos=mesh.world*vec4f(vertexInputs.position,1.0);vertexOutputs.vWorldPos=worldPos;vertexOutputs.position=scene.viewProjection*worldPos;}
-`;e.ShadersStoreWGSL[t]||(e.ShadersStoreWGSL[t]=r);const a=[n,s];for(const o of a)e.IncludesShadersStoreWGSL[o.name]||(e.IncludesShadersStoreWGSL[o.name]=o.shader);const c={name:t,shader:r};export{c as volumetricLightingRenderVolumeVertexShaderWGSL};
