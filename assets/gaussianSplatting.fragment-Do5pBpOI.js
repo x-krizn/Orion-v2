608accd3d449e1c628bc89e3a8c08e98f42fe53f
x-krizn <x.krizn@gmail.com>
2026-06-06 20:14:46 +0000

    Deploying to gh-pages from @ x-krizn/Orion-v2@874575e0661b0f83c1f609fc3760a657ec0b39a0 🚀

---
+++ assets/gaussianSplatting.fragment-Do5pBpOI.js
@@ -0,0 +1,16 @@
+import{S as a,O as i,ak as r,a1 as o,am as l,a2 as s,a9 as S,Q as g}from"./index-Bi-_BDXi.js";const n="gaussianSplattingPixelShader",t=`#include<clipPlaneFragmentDeclaration>
+#include<logDepthDeclaration>
+#include<fogFragmentDeclaration>
+varying vColor: vec4f;varying vPosition: vec2f;
+#define CUSTOM_FRAGMENT_DEFINITIONS
+#include<gaussianSplattingFragmentDeclaration>
+@fragment
+fn main(input: FragmentInputs)->FragmentOutputs {
+#define CUSTOM_FRAGMENT_MAIN_BEGIN
+#include<clipPlaneFragment>
+var finalColor: vec4f=gaussianColor(input.vColor,input.vPosition);
+#define CUSTOM_FRAGMENT_BEFORE_FRAGCOLOR
+fragmentOutputs.color=finalColor;
+#define CUSTOM_FRAGMENT_MAIN_END
+}
+`;a.ShadersStoreWGSL[n]||(a.ShadersStoreWGSL[n]=t);const c=[i,r,o,l,s,S,g];for(const e of c)a.IncludesShadersStoreWGSL[e.name]||(a.IncludesShadersStoreWGSL[e.name]=e.shader);const m={name:n,shader:t};export{m as gaussianSplattingPixelShaderWGSL};
